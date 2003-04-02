88a85792b0441b3e9876ce79e1ca0b16cb0e83d5
Vince Carey <stvjc@channing.harvard.edu>
2003-04-02 02:23:09 +0000

    improved NELhandler to handle weights fields in GXL; man page for GXL also modified to include an example of extracting weights from a GXL doc
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/graph@3828 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/NELhandler.R
+++ R/NELhandler.R
@@ -1,72 +1,90 @@
-
-NELhandler <- function() {
+NELhandler <- function () 
+{
 #
 # handler for xmlEventParse for the node and edge
 # components of GXL 1.0.1
+#  REVISED 1 apr 03 so that weights come into graphNEL if
+#   present as weights child of edges in GXL
 #
 #
 # local store
 #
-     graphID <- NULL
-     curNode <- NULL
-     curEdge <- NULL
-     curAttr <- NULL
-     inNode <- FALSE
-     inEdge <- FALSE
-     inAttr <- FALSE
-     nodeL <- list()
-     edgeL <- list()
+    graphID <- NULL
+    curNode <- NULL
+    curEdge <- NULL
+    curAttr <- NULL
+    inNode <- FALSE
+    inEdge <- FALSE
+    inAttr <- FALSE
+    nodeL <- list()
+    edgeL <- list()
 #
 # handler elements: start elements are cased for
 #  graph, node, attr, or edge
 # text is limited in the simple example to the attr tag,
 #   which lives under a node or an edge
 #
-     startElement = function(x, atts, ...) {
-      if (x == "graph") graphID <<- atts["id"]
-      else if (x == "node") {
-       inNode <<- TRUE
-       nodeL[[ atts["id"] ]] <<- list()
-       curNode <<- atts["id"]
-      }
-      else if (x == "attr") {
-       inAttr <<- TRUE
-       curAttr <<- atts["name"]
-      }
-      else if (x == "edge") {
-       inNode <<- FALSE
-       inEdge <<- TRUE
-       edgeL[[ atts["id"] ]] <<- list()
-       edgeL[[ atts["id"] ]][["span"]] <<- c(atts["from"],
-                  atts["to"])
-       curEdge <<- atts["id"] 
-      }
-     }
-     text = function(x, atts, ...) {
-       if (inNode & inAttr & nchar(x)>0) 
-              nodeL[[ curNode ]][[ curAttr ]] <<- x
-       else if (inEdge & inAttr & nchar(x)>0) 
-              edgeL[[ curEdge ]][[ curAttr ]] <<- x
-     }
-     endElement = function(x, ...) {
-         if (x == "attr") inAttr <<- FALSE
-         else if (x == "node") inNode <<- FALSE
-         else if (x == "edge") inEdge <<- FALSE
-     }
-     dump = function() {
-        list(graphID=graphID, nodeL=nodeL, edgeL=edgeL)
-     }
-     asGraphNEL = function() {
-       require(graph)
-       src <- sapply(edgeL,function(x)x$span["from"])
-       dest <- sapply(edgeL,function(x)x$span["to"]) 
-       ns <- names(nodeL)
-       bas <- match(src,ns)
-       tar <- match(dest,ns)
-       edl <- list()
-       for (i in 1:length(ns)) edl[[ns[i]]] <- list(edges=tar[bas==i])
-       new("graphNEL", nodes=ns, edgeL=edl, edgemode="directed")
-     }
-     list( startElement=startElement, endElement=endElement,
-                text=text, dump=dump, asGraphNEL=asGraphNEL )
-   }
+    startElement = function(x, atts, ...) {
+        if (x == "graph") 
+            graphID <<- atts["id"]
+        else if (x == "node") {
+            inNode <<- TRUE
+            nodeL[[atts["id"]]] <<- list()
+            curNode <<- atts["id"]
+        }
+        else if (x == "attr") {
+            inAttr <<- TRUE
+            curAttr <<- atts["name"]
+        }
+        else if (x == "edge") {
+            inNode <<- FALSE
+            inEdge <<- TRUE
+            edgeL[[atts["id"]]] <<- list()
+            edgeL[[atts["id"]]][["span"]] <<- c(atts["from"], 
+                atts["to"])
+            curEdge <<- atts["id"]
+        }
+    }
+    text = function(x, atts, ...) {
+        if (inNode & inAttr & nchar(x) > 0) 
+            nodeL[[curNode]][[curAttr]] <<- x
+        else if (inEdge & inAttr & nchar(x) > 0) 
+            edgeL[[curEdge]][[curAttr]] <<- c(edgeL[[curEdge]][[curAttr]], 
+                x)
+    }
+    endElement = function(x, ...) {
+        if (x == "attr") 
+            inAttr <<- FALSE
+        else if (x == "node") 
+            inNode <<- FALSE
+        else if (x == "edge") 
+            inEdge <<- FALSE
+    }
+    dump = function() {
+        list(graphID = graphID, nodeL = nodeL, edgeL = edgeL)
+    }
+    asGraphNEL = function() {
+#
+# just handles weights children of edges when present, needs to handle
+# arbitrary children of edges and nodes
+#
+        require(graph)
+        ns <- names(nodeL)
+        src <- sapply(edgeL, function(x) x$span["from"])
+        dest <- sapply(edgeL, function(x) x$span["to"])
+        wts <- sapply(edgeL, function(x) x[["weights"]])
+        IGNWTS <- FALSE
+        if (any(sapply(wts,function(x)is.null(x)))) IGNWTS <- TRUE
+        if (!IGNWTS) sw <- split(wts, src)
+        edl <- split(dest, src)
+        nne <- names(edl)
+        nl <- list()
+        if (!IGNWTS) for (i in 1:length(edl)) nl[[nne[i]]] <- list(edges = match(edl[[i]], 
+            ns), weights = as.numeric(sw[[i]]))
+        else for (i in 1:length(edl)) nl[[nne[i]]] <- list(edges = match(edl[[i]], 
+            ns))
+        new("graphNEL", nodes = ns, edgeL = nl, edgemode = "directed")
+    }
+    list(startElement = startElement, endElement = endElement, 
+        text = text, dump = dump, asGraphNEL = asGraphNEL)
+}
