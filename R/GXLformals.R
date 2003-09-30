a47ee9d4f2903f856030122159a60eb19c266f70
Vince Carey <stvjc@channing.harvard.edu>
2003-09-30 02:51:10 +0000

    minor mods to omit bad DTD record emission and add edgemode attr in toGXL
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/graph@5847 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/GXLformals.R
+++ R/GXLformals.R
@@ -42,12 +42,12 @@ gxlTreeNEL <- function(gnel) {
  nds <- nodes(gnel)
  eds <- lapply(edges(gnel),unique)
  enms <- names(eds)
- out <- xmlTree("gxl", dtd="http://www.gupro.de/GXL/gxl-1.0.1.dtd",
+ out <- xmlTree("gxl", #dtd="http://www.gupro.de/GXL/gxl-1.0.1.dtd",
    namespaces=c(gxl="http://www.w3.org/1999/xlink"))
 #<!DOCTYPE gxl SYSTEM "http://www.gupro.de/GXL/gxl-1.0.1.dtd">
 #<gxl xmlns:xlink="http://www.w3.org/1999/xlink">
  out$addTag("gxl",close=FALSE)
- out$addTag("graph", attrs=c(id="graphNEL"), close=FALSE)
+ out$addTag("graph", attrs=c(id="graphNEL", edgemode=edgemode(gnel)), close=FALSE)
  for (i in 1:length(nds))
    {
    out$addTag("node", attrs=c(id=nds[i]), close=FALSE)
