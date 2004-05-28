b5c13e35124126a0b336f30a3cc01be83c3d7eea
saikat <unknown>
2004-05-28 22:32:54 +0000

    Make the propertyHolder class virtual. add some error checking in the property get/set methods. Add another asGraphProperty method with x numeric and hasWeight missing.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/graph@8891 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/property.R
+++ R/property.R
@@ -1,5 +1,6 @@
 setClass("propertyHolder",
-         representation(property="list"))
+         representation(property="list"),
+         contains="VIRTUAL")
 
 if (!isGeneric("property"))
     setGeneric("property",
@@ -8,7 +9,12 @@ if (!isGeneric("property"))
 
 setMethod("property",
           signature(x="propertyHolder", prop="character"),
-          function (x, prop) x@property[[prop]])
+          function (x, prop)
+      {
+          if (!(prop %in% names(x@property)))
+              stop(paste("no property named", prop))
+          x@property[[prop]]
+      })
 
 if (!isGeneric("property<-"))
     setGeneric("property<-",
@@ -16,10 +22,14 @@ if (!isGeneric("property<-"))
                standardGeneric("property<-"))
 
 setReplaceMethod("property",
-                 signature(x = "propertyHolder"),
+                 signature(x = "propertyHolder",
+                           prop = "character"),
                  function (x, prop, value)
              {
-                 x@property[[prop]]
+                 if (!(prop %in% names(x@property)))
+                     stop(paste("no property named", prop))
+                 x@property[[prop]] <- value
+                 x
              })
 
 if (!isGeneric("asGraphProperty"))
@@ -66,5 +76,17 @@ setMethod("asGraphProperty",
                    any(is.na(names(x)))))
                   stop("property list must have names for all elements")
           }
+          x
+      })
+
+setMethod("asGraphProperty",
+          signature(x="numeric", hasWeight="missing"),
+          function (x, hasWeight)
+      {
+          x <- as.list(x)
+          if (length(x) != 0 &&
+              (length(names(x)) != length(x) ||
+               any(is.na(names(x)))))
+              stop("property list must have names for all elements")
           x
       })
