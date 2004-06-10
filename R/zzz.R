63e64a0b4960e650eb316cc9023ecd28daad3ad4
Jeff Gentry <jgentry@jimmy.harvard.edu>
2004-06-10 17:12:11 +0000

    fix addVig2Menu so it doesn't throw a warning
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/graph@9043 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -5,8 +5,10 @@
     where = match(paste("package:", pkgname, sep=""), search())
     .initGContents(where)
 
-    if(.Platform$OS.type == "windows" && require("Biobase") && interactive()
-        && .Platform$GUI ==  "Rgui"){
-        addVigs2WinMenu("graph")
-    }
+ if((.Platform$OS.type == "windows") && ("Biobase" %in% installed.packages()[,"Package"])
+    && (interactive()) && (.Platform$GUI ==  "Rgui")){
+     if (require("Biobase"))
+         addVigs2WinMenu("Ruuid")
+ }
+
 }
