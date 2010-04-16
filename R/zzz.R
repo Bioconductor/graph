75d91bf33cf3eeb994a5b934514e63560079e181
Seth Falcon <sfalcon@fhcrc.org>
2010-04-16 22:08:12 +0000

    Remove .onLoad function that isn't needed
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/graph@46119 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,3 @@
-
 ## unexported environment for persistent stuff like par settings
 ## specific to the graph package
 
@@ -6,15 +5,7 @@
 .GraphEnv$par <- list()
 graph.par(.default.graph.pars())
 
-
-.onLoad <- function(libname, pkgname) {
-    if (!require("methods"))
-       stop("Unable to load 'methods' package")
-}
-
 .onUnload <- function( libpath ) {
   library.dynam.unload("BioC_graph", libpath )
 }
-
-
 
